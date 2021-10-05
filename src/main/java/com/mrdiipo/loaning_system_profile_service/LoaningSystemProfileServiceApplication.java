2903d731e9a5b3f05fd170dac13fa5ee0d95c9df
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-05 16:04:59 +0100

    Initial commit

---
+++ src/main/java/com/mrdiipo/loaning_system_profile_service/LoaningSystemProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.loaning_system_profile_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaningSystemProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaningSystemProfileServiceApplication.class, args);
+    }
+
+}
